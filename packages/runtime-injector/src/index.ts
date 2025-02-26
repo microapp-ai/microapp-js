79fad03510c75590d5dd95d96a0eac7cbb508a49
Dhyego Calota <dhyegofernando@gmail.com>
2025-02-26 14:02:28 -0300

    so we fixed runtime-injector

--- packages/runtime-injector/src/index.ts
+++ packages/runtime-injector/src/index.ts
@@ -11,70 +11,112 @@ const buildRoutingScript = getRoutingScriptBuilder();
 export default {
   async fetch(request, env, ctx): Promise<Response> {
     const response = await fetch(request);
-    const url = new URL(request.url);
-    let targetOriginUrl: URL | null = null;
-    let targetOrigin = url.searchParams.get(
-      MICROAPP_URL_PARAM_NAMES.TARGET_ORIGIN
-    );
-
-    targetOrigin = targetOrigin ? decodeURIComponent(targetOrigin) : null;
-
-    try {
-      if (targetOrigin) {
-        targetOriginUrl = new URL(targetOrigin);
-      }
-    } catch (e) {
-      console.error('Invalid target origin URL', e);
-    }
-
     const contentType = response.headers.get('content-type');
-    const shouldInjectScripts =
-      ALLOWED_MICROAPP_ORIGIN_HOSTNAMES.filter(
-        (hostname) =>
-          url.hostname === hostname || url.hostname.endsWith(`.${hostname}`)
-      ).length > 0 &&
-      targetOriginUrl &&
-      ALLOWED_MICROAPP_ORIGIN_HOSTNAMES.includes(targetOriginUrl.hostname) &&
-      contentType &&
-      contentType.includes('text/html');
+    const isHtmlContentType =
+      !!contentType && contentType.includes('text/html');
 
-    if (!shouldInjectScripts) {
+    if (!isHtmlContentType) {
       return response;
     }
 
-    targetOrigin = targetOriginUrl!.origin;
-    let html = await response.text();
+    const requestUrl = new URL(request.url);
+    const isUrlTargetedToInjectUrl =
+      ALLOWED_MICROAPP_ORIGIN_HOSTNAMES.filter(
+        (allowedHostname) =>
+          requestUrl.hostname === allowedHostname ||
+          requestUrl.hostname.endsWith(`.${allowedHostname}`)
+      ).length > 0;
 
-    html = html.replace(
-      '<head>',
-      `<head><script data-target-origin="${targetOrigin}">${buildRoutingScript({
-        targetOrigin,
-      })}</script>`
-    );
+    const targetOriginUrl: URL | null =
+      getAllowedTargetOriginUrlByRequest(request);
 
-    html = html.replace(
-      '<body>',
-      `<body><script data-target-origin="${targetOrigin}">${buildResizingScript(
-        { targetOrigin }
-      )}</script>`
-    );
+    const isTargetOriginUrlAllowed =
+      !!targetOriginUrl &&
+      ALLOWED_MICROAPP_ORIGIN_HOSTNAMES.includes(targetOriginUrl.hostname);
 
     const headers = new Headers(response.headers);
     headers.delete('X-Frame-Options');
     headers.set(
       'Content-Security-Policy',
-      buildContentSecurityPolicyHeader(targetOriginUrl!)
+      buildContentSecurityPolicyHeader({
+        targetOriginUrl,
+        isTargetOriginUrlAllowed,
+      })
+    );
+
+    const shouldInjectScripts =
+      isUrlTargetedToInjectUrl && isTargetOriginUrlAllowed;
+
+    if (!shouldInjectScripts) {
+      return new Response(response.body, {
+        headers,
+        status: response.status,
+      });
+    }
+
+    const targetOrigin = targetOriginUrl.origin;
+    const routingScript = buildRoutingScript({ targetOrigin });
+    const resizingScript = buildResizingScript({ targetOrigin });
+
+    let html = await response.text();
+
+    html = html.replace(
+      '<head>',
+      `<head><script data-target-origin="${targetOrigin}">${routingScript}</script>`
+    );
+
+    html = html.replace(
+      '<body>',
+      `<body><script data-target-origin="${targetOrigin}">${resizingScript}</script>`
     );
 
     return new Response(html, {
       headers,
+      status: response.status,
     });
   },
 } satisfies ExportedHandler<Env>;
 
-function buildContentSecurityPolicyHeader(targetOriginUrl: URL) {
-  // NB: The target origin below is authorized to embed the microapp.
-  const allowedOrigins = [targetOriginUrl.origin];
+function buildContentSecurityPolicyHeader({
+  targetOriginUrl,
+  isTargetOriginUrlAllowed,
+}: {
+  targetOriginUrl: URL | null;
+  isTargetOriginUrlAllowed: boolean;
+}) {
+  const allowedOrigins = [];
+
+  if (targetOriginUrl && isTargetOriginUrlAllowed) {
+    allowedOrigins.push(targetOriginUrl.origin);
+  }
+
+  if (allowedOrigins.length === 0) {
+    return `frame-ancestors 'self'`;
+  }
 
   return `frame-ancestors 'self' ${allowedOrigins.join(' ')}`;
+}
+
+function getAllowedTargetOriginUrlByRequest(request: Request): URL | null {
+  const requestUrl = new URL(request.url);
+  const targetOrigin = requestUrl.searchParams.get(
+    MICROAPP_URL_PARAM_NAMES.TARGET_ORIGIN
+  );
+
+  if (!targetOrigin) {
+    return null;
+  }
+
+  let targetOriginUrl: URL | null = null;
+
+  try {
+    targetOriginUrl = new URL(decodeURIComponent(targetOrigin));
+  } catch (e) {
+    console.error(
+      '[runtime-injector] Invalid target origin URL:',
+      targetOrigin
+    );
+  }
+
+  return targetOriginUrl;
 }
